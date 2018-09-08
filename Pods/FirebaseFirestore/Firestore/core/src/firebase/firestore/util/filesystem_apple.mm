5f613a3b0435641e7933d2588551e2e994cdfe71
Bradley Walters <bwalters@anonyome.com>
2018-09-08 16:08:33 -0600

    Add pods

--- Pods/FirebaseFirestore/Firestore/core/src/firebase/firestore/util/filesystem_apple.mm
+++ Pods/FirebaseFirestore/Firestore/core/src/firebase/firestore/util/filesystem_apple.mm
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Google
+ * Copyright 2018 Google
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,17 +14,32 @@
  * limitations under the License.
  */
 
+#include "Firestore/core/src/firebase/firestore/util/filesystem.h"
+
+#if defined(__APPLE__)
+
 #import <Foundation/Foundation.h>
 
-#import "Firestore/Source/Local/FSTGarbageCollector.h"
+namespace firebase {
+namespace firestore {
+namespace util {
 
-NS_ASSUME_NONNULL_BEGIN
+Path TempDir() {
+  const char* env_tmpdir = getenv("TMPDIR");
+  if (env_tmpdir) {
+    return Path::FromUtf8(env_tmpdir);
+  }
 
-/**
- * A garbage collector implementation that does absolutely nothing. It ignores all
- * addGarbageSource: and addPotentialGarbageKey: messages and never produces any garbage.
- */
-@interface FSTNoOpGarbageCollector : NSObject <FSTGarbageCollector>
-@end
+  NSString* ns_tmpdir = NSTemporaryDirectory();
+  if (ns_tmpdir) {
+    return Path::FromNSString(ns_tmpdir);
+  }
 
-NS_ASSUME_NONNULL_END
+  return Path::FromUtf8("/tmp");
+}
+
+}  // namespace util
+}  // namespace firestore
+}  // namespace firebase
+
+#endif  // defined(__APPLE__)
